0cf1339b59cbba4c83184d7073489ee0d5df96d9
ahmed <ahmedhammam056@gmail.com>
2024-07-02 20:18:51 +0300

    finsih sign_up feature

--- lib/core/network/api_service.dart
+++ lib/core/network/api_service.dart
@@ -2,17 +2,18 @@ import 'package:dio/dio.dart';
 import 'package:docdoc_project/core/network/api_constants.dart';
 import 'package:docdoc_project/features/login/data/model/login_request.dart';
 import 'package:docdoc_project/features/login/data/model/login_response.dart';
+import 'package:docdoc_project/features/sign_up/data/models/sign_up_request.dart';
+import 'package:docdoc_project/features/sign_up/data/models/sign_up_response.dart';
 import 'package:retrofit/http.dart';
 part 'api_service.g.dart';
 
 @RestApi(baseUrl: ApiConstants.baseUrl)
 abstract class ApiService {
-  
-  factory ApiService(Dio dio,{String baseUrl}) = _ApiService;
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
 
   @POST(ApiConstants.login)
-  Future<LoginResponse> login(
-    @Body() LoginRequest loginRequest
-    );
+  Future<LoginResponse> login(@Body() LoginRequest loginRequest);
 
+  @POST(ApiConstants.signUp)
+  Future<SignUpResponse> signUp(@Body() SignUpRequest signUpRequest);
 }
